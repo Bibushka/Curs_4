0daa9faa721bcda749a91c6413c4b127e96b09c5
Bibushka <bianca_bucur03@yahoo.com>
2017-05-30 22:49:40 +0300

    Binary test: red

---
+++ Binary/Binary/Binary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Binary
+{
+    [TestClass]
+    public class Binary
+    {
+        [TestMethod]
+        {
+            CollectionAssert.AreEqual(new byte[] {0,0,0,1}, ReturnBytes(1));
+        }
+
+        public Array ReturnBytes(int number)
+        {
+            return new byte[] {0,0,0,0};
+        }
+    }
+}
