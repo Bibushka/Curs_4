a7a5ce1ccea2537d1bc2d8b865cf319f8f436cee
Bibushka <bianca_bucur03@yahoo.com>
2017-06-06 19:48:35 +0300

    Binary test: red_2
    Message: Assert.AreEqual failed. Expected: <System.Byte[]>. Actual:
    <System.Byte[]>

--- Binary/Binary/Binary.cs
+++ Binary/Binary/Binary.cs
@@ -9,15 +9,12 @@ namespace Binary
         [TestMethod]
         public void GetBinaryForm()
         {
-            Assert.AreEqual(new byte[] {1}, ReturnBytes(1));
         }
 
         public byte[] ReturnBytes(int number)
         {
             byte[] yourByte= { };
-            while (number!=0)
             {
-                Array.Resize(ref yourByte, 1);
                 yourByte[yourByte.Length-1] = Convert.ToByte(number % 2);
                 number = number / 2;
             }
